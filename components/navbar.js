517c0a79b0a6e80759c3969fc3f5e3de7ba874ee
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2022-08-24 22:26:24 +0530

    navbar&index page

---
+++ components/navbar.js
@@ -0,0 +1,37 @@
+function navbar() {
+    return `<div id="kau_upper">
+    <div>
+        <a href="">
+            <img src="	https://www.kindmeal.my/images/logo-kindmeal.png" id="kau_logo" alt="">
+        </a>
+    </div>
+    <div>
+        <a href="">
+            <img src="https://www.kindmeal.my/images/follow_blog_grey.png" id="kau_link" alt="">
+        </a>
+        <a href="">
+            <img src="	https://www.kindmeal.my/images/follow_facebook_grey.png" id="kau_fb" alt="">
+        </a>
+        <a href="">
+            <img src="	https://www.kindmeal.my/images/follow_twitter_grey.png" id="kau_tw" alt="">
+        </a>
+    </div>
+    <div>
+        <a href="" id="kau_login"><p>Login</p></a>
+        <a href="" id="kau_facebook"><button>Facebook</button></a>
+        <a href="" id="kau_email"><button>Email</button></a>
+        <a href="" id="kau_signup"><p>Sign up</p></a>
+    </div>
+</div>
+<div id="kau_lower">
+    <a href="">Home</a>
+    <a href="">Meal Deals</a>
+    <a href="">KindMoments</a>
+    <a href="">Hot Picks</a>
+    <a href="">Recipies</a>
+    <a href="">Directory</a>
+    <a href="">Articles</a>
+    <a href="">Help</a>
+</div>`;
+}
+export {navbar};

9967de13a983cfe3e76edee1048b448617ca2f71
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2022-08-25 22:23:19 +0530

    About KindMeal and Advertising Page added

---
+++ kirti_Script/advertising.js
@@ -0,0 +1,10 @@
+import {navbar,menubar} from "../kirti_components/navbar.js";
+
+document.getElementById("navbar").innerHTML = navbar()
+document.getElementById("menubar").innerHTML = menubar()
+
+
+import sidebar from "../kirti_components/sidebar.js";
+
+document.getElementById("sidebar-section").innerHTML = sidebar()
+

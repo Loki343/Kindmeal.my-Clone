2c74f0d89bd333d15dbc221b399ebcd36930d86f
Nitin potbhare <potbharenitin6684@gmail.com>
2022-08-26 23:11:02 +0530

    Add day 4 task

--- components/navbar.js
+++ components/navbar.js
@@ -1,37 +1,37 @@
 function navbar() {
   return `<div id="kau_upper">
-    <div>
-        <a href="">
-            <img src="	https://www.kindmeal.my/images/logo-kindmeal.png" id="kau_logo" alt="">
-        </a>
-    </div>
-    <div>
-        <a href="">
-            <img src="https://www.kindmeal.my/images/follow_blog_grey.png" id="kau_link" alt="">
-        </a>
-        <a href="">
-            <img src="	https://www.kindmeal.my/images/follow_facebook_grey.png" id="kau_fb" alt="">
-        </a>
-        <a href="">
-            <img src="	https://www.kindmeal.my/images/follow_twitter_grey.png" id="kau_tw" alt="">
-        </a>
-    </div>
-    <div>
-    <label for="show"><a href="login.html" id="kau_login">Login</a></label>
-        <a href="" id="kau_facebook"><button>Facebook</button></a>
-        <a href="" id="kau_email"><button>Email</button></a>
-        <label for="show"><a href="main.html" id="kau_signup">Sign up</a></label>
-    </div>
-</div>
-<div id="kau_lower">
-    <a href="">Home</a>
-    <a href="">Meal Deals</a>
-    <a href="">KindMoments</a>
-    <a href="">Hot Picks</a>
-    <a href="">Recipies</a>
-    <a href="">Directory</a>
-    <a href="">Articles</a>
-    <a href="">Help</a>
-</div>`;
+      <div>
+          <a href="">
+              <img src="	https://www.kindmeal.my/images/logo-kindmeal.png" id="kau_logo" alt="">
+          </a>
+      </div>
+      <div>
+          <a href="">
+              <img src="https://www.kindmeal.my/images/follow_blog_grey.png" id="kau_link" alt="">
+          </a>
+          <a href="">
+              <img src="	https://www.kindmeal.my/images/follow_facebook_grey.png" id="kau_fb" alt="">
+          </a>
+          <a href="">
+              <img src="	https://www.kindmeal.my/images/follow_twitter_grey.png" id="kau_tw" alt="">
+          </a>
+      </div>
+      <div>
+      <label for="show"><a href="" id="kau_login">Login</a></label>
+          <a href="" id="kau_facebook"><button>Facebook</button></a>
+          <a href="" id="kau_email"><button>Email</button></a>
+          <label for="show"><a href="main.html" id="kau_signup">Sign up</a></label>
+      </div>
+  </div>
+  <div id="kau_lower">
+      <a href="">Home</a>
+      <a href="">Meal Deals</a>
+      <a href="">KindMoments</a>
+      <a href="">Hot Picks</a>
+      <a href="">Recipies</a>
+      <a href="">Directory</a>
+      <a href="">Articles</a>
+      <a href="">Help</a>
+  </div>`;
 }
 export { navbar };

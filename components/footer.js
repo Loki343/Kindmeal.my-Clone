813b3f8be632b14fa00f46cff2015a877341a91f
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2022-08-26 21:14:07 +0530

    landingPage,articlePage

---
+++ components/footer.js
@@ -0,0 +1,74 @@
+function footer1(){
+    return`<div style="width: 63%;display: flex;">
+    <div class="kau_footerfourdiv">
+        <h4>General</h4>
+        <p>Home</p>
+        <p>Sign Up</p>
+        <p>Businesses / Restaurateurs</p>
+        <p>Advertising</p>
+        <p>About KindMeal.my</p>
+        <p>Help & FAQ</p>
+        <p>Terms & Conditions</p>
+        <p>Inspiring Partners</p>
+        <p>Lifestyle Ambassadors</p>
+        <p>Jobs & Careers</p>
+        <p>Contact Us</p>
+    </div>
+    <div class="kau_footerfourdiv">
+        <h4>Features</h4>
+        <p>Meat-Free Deals</p>
+        <p>Tasty Menus</p>
+        <p>Kind Moments</p>
+        <p>Meat-Free Recipes</p>
+        <p>Member Recommendations</p>
+        <p>Featured Restaurants</p>
+        <p>Vegetarian & Vegan Directory</p>
+        <p>Food Map</p>
+        <p>Become A Superhero</p>
+        <p>Vegan News & Vegetarian Articles</p>
+        <p>Latest Comments</p>
+    </div>
+    <div class="kau_footerfourdiv">
+        <h4>Social Media</h4>
+        <p>KindMeal Widget</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+    </div>
+    <div class="kau_footerfourdiv">
+        <h4>Mobile</h4>
+        <p>iPhone & iPod App</p>
+        <p>Android App</p>
+        <h4 style="margin-top: 30px;">Exciting Promos</h4>
+        <p>Gadhimai: Ending Mass</p>
+        <p>Free Meals</p>
+        <p>Food Bloggers</p>
+        <p>Uber CHIRP</p>
+        <p>Jane Goodall Contest</p>
+        <p>Win Digi iPhone 6</p>
+        <p>Feed The Poor</p>
+        <p>Win Superhero Gifts</p>
+        <p>Win an iPad Mini 3</p>
+    </div>
+    <div class="kau_footerfourdiv">
+        <h4>PetFinder.my</h4>
+        <p>Adopt A Pet</p>
+        <p>Smartphone Apps</p>
+        <p>WAGazine</p>
+        <p>Discussion Forum</p>
+        <p>Medical Fund</p>
+    </div>
+</div>`;
+}
+
+function footer2(){
+    return `<div style="width: 75%;display: flex;justify-content: center;">
+    <div>
+        <p>Copyright © KindMeal.my, 2014 - 2022. All rights reserved.</p>
+        <p>This website promotes compassionate, meat-free dining experience. Some food may contain eggs, dairy
+            products or alcohol, please view individual listings for details.</p>
+    </div>
+</div>`;
+}
+
+export {footer1,footer2} ;
